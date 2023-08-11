d43de3d321b736f9062954380d6632dbb6ec9a1d
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-08-11 10:29:57 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - A C program that prints all single digit numbers of base 10
+ * main - A C program that prints all digits starting from base 10
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int lowercase = 'a';
-		while (lowercase <= 'z')
-	{
-		putchar(lowercase);
-		lowercase += 1;
-	}
-	putchar('\n');
+	int num = 0;
+		while (num < '10')
+		{
+			printf("%d", num);
+			num += 1;
+		}
+	printf("\n");
 	return (0);
 }
