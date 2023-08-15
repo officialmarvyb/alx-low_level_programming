17e7b37dbff9d9bfc896b3b78111699198ba61fa
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-08-15 09:56:41 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - check the code.
+ * @c: The character to be checked
+ *
+ * Return: 1 for lowercase character or 0 for failure
+ */
+int _isalpha(int c)
+
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
