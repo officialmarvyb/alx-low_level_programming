37222e89a5511be0d4f49a85bbf2d1d1af304055
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-08-15 14:38:03 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - A function that prints all natural numbers
@@ -29,11 +30,11 @@ void print_to_98(int n)
 	}
 	else
 	{
-	for (; n >= 98; n ==)
+	for (; n >= 98; n--)
 	{
 	if (n == 98)
 	{
-	printf("%d", n);
+	printf("%d" , n);
 	printf("\n");
 	break;
 	}
