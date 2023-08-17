97d47e78d54af1ccd8ca68d7c7c490e3ca4c2b07
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-08-17 02:35:00 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9\n
+ *
+ * Return: Void
+ */
+
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	_putchar(i + '0');
+	}
+	_putchar('\n');
+}
