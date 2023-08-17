ee12f2c4b604c5ef323bc7584dfbd7a5ffca62c2
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-08-16 23:37:26 -0700

    Is_upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,13 @@
 /**
  * _isupper - A function that checks for uppercase characters
  * @c: int type number
- * Return: 1 if val is uppercase, else 0
+ * Return: 1 for uppercase, else 0
  */
 int _isupper(int c)
 {
-	if (c > 64 && c < 91)
-		return (1);
-
+	if (c >= 65 && c < 91)
+	{
+	return (1);
+	}
 	return (0);
 }
