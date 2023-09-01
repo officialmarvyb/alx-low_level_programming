f76af5dc83780f62993e35932a7f5c88d04ad5e7
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-09-01 14:53:09 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add 2 positive numbers and print the result
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Description: If no number is passed to program, print 0.
+ * If one of the numbers contain non-digits, print Error.
+ *
+ * Return: 1 if error, 0 if function runs properly.
+ */
+
+int main(int argc, char *argv[])
+{
+	int total = 0;
+	int num;
+
+	total = 0;
+	if (argc > 1)
+	{
+		for (int i = 1; argc; i++)
+		{
+			char *p;
+
+			num = strtol(argc, &p, 10);
+			if (*p == '\0')
+				total += num;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", total);
+	return (0);
+}
