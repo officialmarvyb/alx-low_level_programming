74af28c7f781623e7b018fe22bde24654c891f24
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-09-04 14:26:37 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strdup - Copy of the string given as a parameter
+* @str: The string given.
+*
+* Return: The pointer to the array.
+*/
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *b;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	;
+	}
+	i = i + 1;
+	b = malloc(sizeof(char) * i);
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+	j = 0;
+	for (j = 0; j < i; j++)
+	{
+		b[j] = str[j]
+	}
+return (b);
+}
