2b673a252e151e233e8c75b0786f5a4e62f6b3b0
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-09-13 08:09:15 -0700

    Print Name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
  * print_name - this functions prints a name
  * @name: string to print.
  * @f: this a function pointer, that recieves a string.
- * Return: this is a void function no return
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
