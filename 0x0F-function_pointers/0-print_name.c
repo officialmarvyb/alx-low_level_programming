67b775a3e772d251cb7e0fd1686af1304f55ae77
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-09-13 08:13:00 -0700

    Print Name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
- * print_name - this functions prints a name
- * @name: string to print.
- * @f: this a function pointer, that recieves a string.
- * Return: void
- */
+  * print_name - this functions prints a name
+  * @name: string to print.
+  * @f: this a function pointer, that recieves a string.
+  * Return: void
+  */
 
 void print_name(char *name, void (*f)(char *))
 {
