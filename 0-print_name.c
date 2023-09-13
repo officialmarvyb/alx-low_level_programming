8f367b202ed599b824a46bc8150b20358c994b3f
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-09-13 05:23:23 -0700

    What's my name

---
+++ 0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - this functions prints a name
+ * @name: string to print.
+ * @f: this a function pointer, that recieves a string.
+ * Description: this function prints a name)?
+ * section header: the header of this function is function_pointers.h)*
+ * Return: this is a void function with no return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);/*Call the provided function pointer to print the name*/
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-alx low level programming
+C - Function pointers
