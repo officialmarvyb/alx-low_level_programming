9f32a646fe7d331303fe76fb3130e44427b4b981
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-09-14 08:09:10 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,41 +7,33 @@
  */
 
 void print_all(const char * const format, ...)
-	{
-	va_list args; /*Declare a variable to hold the arguments*/
-	unsigned int i = 0;
+{
+	va_list args;
 	char *separator = "";
+	int i = 0;
+	char *str;
 
 	va_start(args, format);
-	/*Initialize va_list with the variable number of arguments*/
 
 	while (format && format[i])
 	{
-	switch (format[i])
-	{
-	case 'c': /*Character*/
-		printf("%s%c", separator, va_arg(args, int));
-		break;
-	case 'i': /* Integer */
-		printf("%s%d", separator, va_arg(args, int));
-		break;
-	case 'f': /* Float */
-		printf("%s%f", separator, va_arg(args, double));
-		break;
-	case 's': /* String */
-	{
-		char *str = va_arg(args, char *);
-
-		if (str == NULL)
-			printf("%s(nil)", separator);
-		else
+		if (format[i] == 'c')
+			printf("%s%c", separator, va_arg(args, int));
+		else if (format[i] == 'i')
+			printf("%s%d", separator, va_arg(args, int));
+		else if (format[i] == 'f')
+			printf("%s%f", separator, va_arg(args, double));
+		else if (format[i] == 's')
+		{
+			str = va_arg(args, char *);
+			if (str == NULL)
+				str = "(nil)";
 			printf("%s%s", separator, str);
-	}
-	break;
-	}
-	separator = ", "; /*Set separator for subsequent values*/
+		}
+
+	separator = ", ";
 	i++;
 	}
-	printf("\n"); /*Print a new line at the end*/
-	va_end(args); /* Clean up the va_list*/
+	printf("\n");
+	va_end(args);
 }
