b0c9b739cdc61642983889d4ee13cb9f45536e40
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-09-14 08:50:11 -0700

    Marv

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,26 +7,9 @@
 #include <string.h>
 #include <stdarg.h>
 
-/**
- * struct specs - structure of specifiers and their printf ones
- * @spec: given specifiers
- * @func : printf corresponding function
- */
-
-typedef struct specs
-{
-	char *spec;
-	void (*func)(va_list);
-} specs_t;
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-void print_char(va_list list);
-void print_int(va_list list);
-void print_float(va_list list);
-void print_string(va_list list);
 
 #endif /* VARIADIC_FUNCTIONS_H */
