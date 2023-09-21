b6ee9892929c6b117794baa62b9b84afea4e30af
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-09-21 09:57:08 -0700

    Requirement check

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,16 @@
 section .data
-    msg: db "Hello, Holberton",10,0 ; The string to be printed, followed by a new line and null terminator
-    format: db "%s",0                ; Format specifier for printf
+	msg: db "Hello, Holberton",10,0 ; The string to be printed, followed by a new line and null terminator
+	format: db "%s",0                ; Format specifier for printf
 
 section .text
-    global main
-    extern printf
+	global main
+	extern printf
 
 main:
-    push rbp                        ; Save base pointer
-    mov rdi, format                ; Load format specifier into rdi
-    mov rsi, msg                 ; Load address of the string into rsi
-    call printf                    ; Call the printf function
+	push rbp                        ; Save base pointer
+	mov rdi, format                ; Load format specifier into rdi
+	mov rsi, msg                 ; Load address of the string into rsi
+	call printf                    ; Call the printf function
 
-    pop rbp                         ; Restore base pointer
-    ret                            ; Return from main
+	pop rbp                         ; Restore base pointer
+	ret                            ; Return from main
