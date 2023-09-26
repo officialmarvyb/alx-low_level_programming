7840eb2e816d1ec017b13099943c81db4eeaa335
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-09-26 04:23:16 -0700

    Task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -6,23 +8,36 @@
  *
  * Return: The number of nodes in the list.
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t cnode = 0;/*Initialize a counter for nodes*/
+    const listint_t *current;
+    size_t count = 0;
+    const listint_t *nodes[10000]; /* Assuming a maximum of 10,000 nodes */
 
-	while (head != NULL)/*Iterate thr linked list & print Val*/
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		cnode++;
-		head = head->next;/*Move head pointer to the next node */
+    current = head;
+    while (current != NULL)
+    {
+        size_t i;
 
-		/* Check if a loop is reached*/
-		if (head != NULL && head->next >= head)
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			break;
-		}
-	}
-	return (cnode);
+        /* Check for a loop */
+        for (i = 0; i < count; i++)
+        {
+            if (current == nodes[i])
+            {
+                printf("-> [%p] %d\n", (void *)current, current->n);
+                return (count);
+            }
+        }
+
+        /* Print current node */
+        printf("[%p] %d\n", (void *)current, current->n);
+
+        /* Add current node to the array */
+        nodes[count++] = current;
+
+        current = current->next;
+    }
+
+    return (count);
 }
+
