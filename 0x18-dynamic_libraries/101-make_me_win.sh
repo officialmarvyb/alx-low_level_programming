6087460e2997a602b42a5cfce0349fcbb5c858b2
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-11-13 12:42:20 -0800

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/thecodeghinux/alx-low_level_programming/raw/main/0x18-dynamic_libraries/win.so
+wget -P /tmp https://github.com/officialmarvyb/alx-low_level_programming/raw/main/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
